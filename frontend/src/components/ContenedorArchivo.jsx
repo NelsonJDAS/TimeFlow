3bb76d4aa452afa6bf4927dbb562ebefe56060d8
NelsonJDAS <nelsondeabreu4@gmail.com>
2025-02-21 16:01:14 +0100

    contenedorarchivo finalizado

--- frontend/src/components/ContenedorArchivo.jsx
+++ frontend/src/components/ContenedorArchivo.jsx
@@ -1,12 +1,19 @@
 import { MdOutlineFileDownload } from "react-icons/md";
+import { useEffect, useState } from "react"
 
 const ContenedorArchivo = () => {
-  return (
-    <div className="contenedor-archivo container w-80 mt-4 rounded-3 align-content-center">
-        <i className="row icono-descarga text-info">  <MdOutlineFileDownload/></i>
-    
-    </div>
-  )
+    const [userLoad, setUserLoad] = useState(false);
+
+    useEffect(() => {
+        setUserLoad(true)
+    }, [])
+
+    return (
+        <div className={`contenedor-archivo container w-80 mt-4 rounded-3 align-content-center ${userLoad ? "animacion-contenedor activa" : "animacion-contenedor"}`}>
+            <i className="row icono-descarga text-secondary">  <MdOutlineFileDownload /></i>
+            <p className="text-center text-secondary">Haz click para subir el archivo</p>
+        </div>
+    )
 }
 
 export default ContenedorArchivo
