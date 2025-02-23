8359ab54ab3f07fb2893f11314c12cf0578c5e00
NelsonJDAS <nelsondeabreu4@gmail.com>
2025-02-23 16:13:25 +0100

    horas a enteros finalizados

--- frontend/src/components/ContenedorArchivo.jsx
+++ frontend/src/components/ContenedorArchivo.jsx
@@ -9,21 +9,67 @@ const ContenedorArchivo = () => {
 
     const archivoRef = useRef("");
 
+    function ConvertirHora(hora) {
+        const [horas, minutos] = hora.split(':').map(Number);
+        return horas + minutos / 60;
+    }
+
     const handleArchivo = (event) => {
         const file = event.target.files[0];
         if (file) {
-          pdfToText(file)
-            .then((extractedText) => {
-              setArchivo(extractedText);
-            })
-            .catch((error) => {
-              console.error('Error al extraer el texto del PDF:', error);
-            });
+            pdfToText(file)
+                .then((extractedText) => {
+                    console.log(extractedText)
+                    const textoFiltrado = extractedText.replace(/\b[lL]\b/g, "0_Libre")
+                    .replace(/\b[vV]\b/g, "0_Vacaciones").replace(/(?<!:)\b\d+\b(?!:)/g, "")
+                    
+                    let dias = extractedText.match(/(?<!:)\b\d+\b(?!:)/g);
+
+                    console.log(dias)
+                    const textoDivido = textoFiltrado.split(/\s(?=[a-zA-Z])/);
+
+                    console.log(textoDivido)
+                    
+                    const Usuarios = textoDivido.map((user) => {
+                        if (/\d/.test(user)) {
+                            let UserFiltrado = user.replace(/0_/g, "")
+
+                            const [nombre, ...horas] = UserFiltrado.split(/\s+/)
+
+                            let horasAEnteros = []
+                            for (let i = 0; i < horas.length; i++) {
+                                if (/\d/.test(horas[i])){
+                                    horasAEnteros.push(ConvertirHora(horas[i]))
+                                } else {
+                                    horasAEnteros.push(horas[i])
+                                }
+                            } 
+                            for (let i = 0; i < horasAEnteros.length; i += 2) {
+                                horasAEnteros.push(horasAEnteros.slice(i, i + 2));
+                            }
+
+
+                                return {
+                                    nombre : nombre,
+                                    horas : horasAEnteros.slice(0, -1)
+                                }
+                            
+                        }
+                    })
+
+                    setArchivo({
+                        dias: dias,
+                        Usuarios,
+                    });
+                })
+                .catch((error) => {
+                    console.error('Error al extraer el texto del PDF:', error);
+                });
         }
-      };
+    };
 
 
-    
+
     useEffect(() => {
         setUserLoad(true)
     }, [])
